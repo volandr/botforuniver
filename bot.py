b74f63ea76c9a690c99a93ab1eee3378a732fdd3
volandr <34952271+volandr@users.noreply.github.com>
2020-05-03 00:28:48 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,10 +4,24 @@ from bs4 import BeautifulSoup as bs
 
 bot = telebot.TeleBot('1064508466:AAG2oMd9ZyO4QOwEfNoHyZox98QH2rF5fSs')
 
+global isRunning
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    if not isRunning:
+        chat_id = message.chat.id
+        text = message.text
+        msg = bot.send_message(chat_id, 'Сколько вам лет?')
+        bot.register_next_step_handler(msg, askAge) #askSource
+        isRunning = True
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет')
-
+def askAge(message):
+    chat_id = message.chat.id
+    text = message.text
+    if not text.isdigit():
+        msg = bot.send_message(chat_id, 'Возраст должен быть числом, введите ещё раз.')
+        bot.register_next_step_handler(msg, askAge) #askSource
+        return
+    msg = bot.send_message(chat_id, 'Спасибо, я запомнил что вам ' + text + ' лет.')
+    isRunning = False
 
 bot.polling()
